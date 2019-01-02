cf438cef5523fae7e26715873651b67ca5846b94
Song Tran Van <songtranvan2511@gmail.com>
2019-01-02 19:03:56 +0700

    bug

--- db/migrate/20180620030403_change_column_type_in_products.rb
+++ db/migrate/20180620030403_change_column_type_in_products.rb
@@ -1,5 +1,5 @@
 class ChangeColumnTypeInProducts < ActiveRecord::Migration[5.1]
   def change
-    change_column :products, :product_type, 'integer USING CAST(product_type AS integer)'
+    change_column :products, :product_type, 'integer USING CAST(product_type AS integer)', default: 0
   end
 end
