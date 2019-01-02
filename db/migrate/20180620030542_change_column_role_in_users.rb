9f1273d4f2609428b6b19eb34727f7b870b4318b
Song Tran Van <songtranvan2511@gmail.com>
2019-01-02 20:43:11 +0700

    fix 6

--- db/migrate/20180620030542_change_column_role_in_users.rb
+++ db/migrate/20180620030542_change_column_role_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeColumnRoleInUsers < ActiveRecord::Migration[5.1]
   def change
-    change_column :users, :role, 'integer USING CAST(role AS integer)', default: 0
+    change_column :users, :role, 'integer USING CAST(role AS integer)', 'integer USING CAST(default AS integer)'
   end
 end
